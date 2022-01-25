5c2a85577a0de4537bb883da3b416017ab569086
Dhrupal Kamlesh Desai <desai.dhrupal15@gmail.com>
2022-01-25 06:05:12 -0500

    Initial Commit

--- Lab1_8805252/ViewController.swift
+++ Lab1_8805252/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var appLogo: UIImageView!
+    @IBOutlet weak var output: UILabel!
+    @IBOutlet weak var stepButton: UIButton!
+    
+    var outputValue: Int = 0 {
+        didSet {
+            output.text = String(outputValue);
+        }
+    }
+    
+    var stepIncrement: Int = 1 {
+        didSet {
+            stepButton.setTitle("Step = " + String(stepIncrement), for: .normal)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        reset();
     }
-
-
+    
+    func reset() {
+        outputValue = 0;
+        stepIncrement = 1;
+    }
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        reset();
+    }
+    
+    @IBAction func incrementButton(_ sender: UIButton) {
+        outputValue += stepIncrement;
+    }
+    
+    @IBAction func decrementButton(_ sender: UIButton) {
+        outputValue -= stepIncrement;
+    }
+    
+    @IBAction func stepChange(_ sender: UIButton) {
+        if (stepIncrement == 1) {
+            stepIncrement = 2;
+        } else {
+            stepIncrement = 1;
+        }
+    }
+    
 }
 
